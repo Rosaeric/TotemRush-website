0f7dffa5232d5dda6d217075ee9f3650357b03cb
Rosaeric <rosaeric076@gmail.com>
2025-07-23 16:31:04 -0400

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", () => {
-  const voteButtons = document.querySelectorAll(".vote-button");
-  voteButtons.forEach(btn => {
-    btn.addEventListener("click", () => {
-      alert("Thank you for voting TotemRush!");
-    });
-  });
-});
